d1bc4fbff1bdb0592fddeeebc59513c6203d5473
Eduardo Born <nosachamos@gmail.com>
2019-07-09 17:14:21 -0500

    Setting webpack's libraryTarget to umd

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/build'),
     filename: 'index.js',
-    library: 'formalizer'
+    library: 'formalizer',
+    libraryTarget: 'umd'
   },
   externals: {
     react: {
--- webpack.min.config.js
+++ webpack.min.config.js
@@ -6,7 +6,8 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/build'),
     filename: 'index.min.js',
-    library: 'formalizer'
+    library: 'formalizer',
+    libraryTarget: 'umd'
   },
   externals: {
     react: {
