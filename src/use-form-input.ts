ece39b7caf0997a2ea70af60b8a10ce00ed0b321
Eduardo Born <nosachamos@gmail.com>
2019-08-11 21:34:58 -0500

    Removing unused imports

--- src/use-form-input.ts
+++ src/use-form-input.ts
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
+import { FormEvent, useEffect, useRef, useState } from 'react';
 import {
   FormInputData,
   FormInputParams,
